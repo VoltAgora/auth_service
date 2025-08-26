f7fbf462eb623398d83160d1860e18eb8e69bf56
jhonestebanviverosramirez-collab <jhonestebanviverosramirez@gmail.com>
2025-08-26 11:26:48 -0500

    Esteban

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from app.adapters.http.routes import router as auth_router
 
 app = FastAPI(title="Auth Service")
-# Hola mundo
+# Hola mundo, Esteban
 
 # Registrar las rutas del microservicio
 app.include_router(auth_router, prefix="/auth")
